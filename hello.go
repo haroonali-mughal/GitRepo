fbb1c7efb9f57b78a8e1eb7985638a1b1880ca49
haroonali-mughal <haroon.ali@cloudplex.io>
2019-07-18 18:51:54 +0500

    upadated

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ import "fmt"
 func main(){
 	fmt.Println("my name is khan")
 	fmt.Println("Sorry! I forgot to say I am not terrorist :D")
+	fmt.Println("now this time I am checking for github")
 
 }
 
